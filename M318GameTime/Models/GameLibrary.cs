20e0cc2842c2f7ef823caa3b0e4629ed3bb73cd7
Karim Hamoudi <karim.hamoudi212@gmail.com>
2021-11-03 10:55:04 +0100

    date ok

--- M318GameTime/Models/GameLibrary.cs
+++ M318GameTime/Models/GameLibrary.cs
@@ -44,11 +44,11 @@ namespace M318GameTime.Models
         /// </summary>
         public void LoadGames()
         {
-            GamesCollection.Add(new Game("If You're Reading This It's Too Late", "Drake", "/M318GameTime;component/Images/ifyourereadingthisitstoolate.jpg","test", "test", "test", "test", "test"));
-            GamesCollection.Add(new Game("In Defense of the Genre", "Say Anything", "/M318GameTime;component/Images/indefenseofthegenre.jpg", "test", "test", "test", "test", "test"));
-            GamesCollection.Add(new Game("Picaresque", "The Decemberists", "/M318GameTime;component/Images/picaresque.jpg", "test", "test", "test", "test", "test"));
-            GamesCollection.Add(new Game("In Evening Air", "Future Islands", "/M318GameTime;component/Images/ineveningair.jpg", "test", "test", "test", "test", "test"));
-            GamesCollection.Add(new Game("You're Gonna Miss It All", "Modern Baseball", "/M318GameTime;component/Images/youregonnamissitall.jpg", "test", "test", "test", "test", "test"));
+            GamesCollection.Add(new Game("If You're Reading This It's Too Late", "Drake", "/M318GameTime;component/Images/ifyourereadingthisitstoolate.jpg","01.01.01", "test", "test", "test", "test"));
+            GamesCollection.Add(new Game("In Defense of the Genre", "Say Anything", "/M318GameTime;component/Images/indefenseofthegenre.jpg", "01.01.01", "test", "test", "test", "test"));
+            GamesCollection.Add(new Game("Picaresque", "The Decemberists", "/M318GameTime;component/Images/picaresque.jpg", "01.01.01", "test", "test", "test", "test"));
+            GamesCollection.Add(new Game("In Evening Air", "Future Islands", "/M318GameTime;component/Images/ineveningair.jpg", "01.01.01", "test", "test", "test", "test"));
+            GamesCollection.Add(new Game("You're Gonna Miss It All", "Modern Baseball", "/M318GameTime;component/Images/youregonnamissitall.jpg", "01.01.01", "test", "test", "test", "test"));
         }
 
         /// <summary>
